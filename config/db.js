51486c6615d81525add0a0f19b543848bd8d6105
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-07 20:29:42 -0500

    refactor db.js to use async await instead of promises

--- config/db.js
+++ config/db.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose.connect(db, {
-        userNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }).then(()=> console.log('MongoDB Connected'))
-    .catch(err => {
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            userNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        console.log('MongoDB Connected..')
+    }
+    catch (err) {
         console.log(err.message);
-        process.exit(1)
-    });
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
