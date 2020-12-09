74cb5f4c6f7700b09acd88dca34e10677baa1d7d
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-08 21:09:41 -0500

    create json web token and send it as response

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 
 const User = require('../models/User');
@@ -44,7 +46,18 @@ async (req,res) => {
 
         await user.save();
 
-        res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
     }
     catch (err){
         console.error(err.message);
