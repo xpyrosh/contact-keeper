51f3e17043fecd374273d50d3bcf31eb20af5efb
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-13 20:05:56 -0500

    created contact context using dummy data and brought it into app.js

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'JJ',
+                email: 'jj@gmail.com',
+                phone: '111-111-1111',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: 'S Wilson',
+                email: 'swil@gmail.com',
+                phone: '211-121-1211',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Hairy Dog',
+                email: 'hdawg@gmail.com',
+                phone: '666-777-8888',
+                type: 'professional'
+            },
+        ]
+    };
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+
+    return (
+        <ContactContext.Provider 
+        value={{
+            contacts: state.contacts
+        }}>
+            { props.children }
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState;
