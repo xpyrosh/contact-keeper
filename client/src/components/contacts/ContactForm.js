0ec82696abdafef6e64f837057c2e59835b57939
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-13 23:17:10 -0500

    added update functionality to contactstate and components

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -4,7 +4,7 @@ import ContactContext from '../../context/contact/contactContext';
 const ContactForm = () => {
     const contactContext = useContext(ContactContext);
 
-    const { addContact, current, clearCurrent } = contactContext;
+    const { addContact, current, clearCurrent, updateContact } = contactContext;
 
     useEffect(() => {
         if(current !== null){
@@ -32,7 +32,13 @@ const ContactForm = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        addContact(contact);
+
+        if(current === null) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
+
         setContact({
             name: '',
             email: '',
