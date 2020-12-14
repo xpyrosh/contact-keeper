ec4fd081491248738ef28d2082498a258842c508
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-14 18:53:20 -0500

    implemented contact filtering

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
                 type: 'professional'
             },
         ],
-        current: null
+        current: null,
+        filtered: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -69,8 +70,14 @@ const ContactState = props => {
     }
 
     // Filter Contacts
+    const filterContacts = text => {
+        dispatch({type: FILTER_CONTACTS, payload:text});
+    }
 
     // Clear Filter
+    const clearFilter = contact => {
+        dispatch({type: CLEAR_FILTER});
+    }
 
 
     return (
@@ -78,11 +85,14 @@ const ContactState = props => {
         value={{
             contacts: state.contacts,
             current: state.current,
+            filtered: state.filtered,
             addContact,
             deleteContact,
             setCurrent,
             clearCurrent,
-            updateContact
+            updateContact,
+            filterContacts,
+            clearFilter
         }}>
             { props.children }
         </ContactContext.Provider>
