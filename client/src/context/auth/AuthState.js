3baf50552d22bda61de24b0c1d0d1622016d0775
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-12-15 21:31:26 -0500

    implemented user registration; using the authContext, register component and our backend api we were successfully able to create a new user from our apps register page and have it show up in mongodb

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -24,14 +25,47 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState);
 
     // LOAD USER
+    const loadUser = () => {
+        console.log('dummy func');
+    }
 
     // REGISTER USER
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users', formData, config);
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            });
+        }
+    };
 
     // LOGIN USER
+    const login = () => {
+        console.log('dummy func');
+    }
 
     // LOGOUT
+    const logout = () => {
+        console.log('dummy func');
+    }
 
     // CLEAR ERRORS
+    const clearErrors = () => {
+        dispatch({type: CLEAR_ERRORS});
+    }
 
 
 
@@ -43,6 +77,11 @@ const AuthState = props => {
             loading: state.loading,
             user: state.user,
             error: state.error,
+            register,
+            loadUser,
+            login,
+            logout,
+            clearErrors
         }}>
             { props.children }
         </AuthContext.Provider>
