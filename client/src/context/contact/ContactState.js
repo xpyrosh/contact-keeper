4d41206fa1e9c84d542fc38d7427b6de496a4c8f
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-01-04 22:32:21 -0500

    adding contacts reflects the DB now, doesnt show added contacts on load as yet. no more hard coded contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -10,22 +10,40 @@ import {
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
-    CLEAR_FILTER
+    CLEAR_FILTER,
+    CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
     const initialState = {
         contacts: [],
         current: null,
-        filtered: null
+        filtered: null,
+        error: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
-    const addContact = contact => {
-        contact.id = uuidv4();
-        dispatch({type: ADD_CONTACT, payload: contact});
+    const addContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/contacts', contact, config);
+            dispatch({
+                type: ADD_CONTACT, 
+                payload: res.data
+            });
+        } catch (error) {
+            dispatch({ 
+                type: CONTACT_ERROR,
+                payload: error.response.msg
+             });
+        }
     }
 
     // Delete Contact
@@ -65,6 +83,7 @@ const ContactState = props => {
             contacts: state.contacts,
             current: state.current,
             filtered: state.filtered,
+            error: state.error,
             addContact,
             deleteContact,
             setCurrent,
