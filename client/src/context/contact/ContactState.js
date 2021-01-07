7650fdd8299caa57eea7130f603eaae6f2f958bb
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-01-07 00:31:55 -0500

    update reflects db

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -77,6 +77,28 @@ const ContactState = props => {
         }
     }
 
+    // Update Contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+            dispatch({
+                type: UPDATE_CONTACT, 
+                payload: res.data
+            });
+        } catch (error) {
+            dispatch({ 
+                type: CONTACT_ERROR,
+                payload: error.response.msg
+             });
+        }
+    }
+
     // Clear Contacts 
     const clearContacts = () => {
         dispatch({ type: CLEAR_CONTACTS });
@@ -90,11 +112,6 @@ const ContactState = props => {
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({type: CLEAR_CURRENT});
-    }
-
-    // Update Contact
-    const updateContact = contact => {
-        dispatch({type: UPDATE_CONTACT, payload: contact});
     }
 
     // Filter Contacts
